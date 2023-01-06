b6141a28138de376a4ff979279483579fe9df095
justinmarotta13 <jamarotta13@gmail.com>
2023-01-06 00:04:50 -0500

    first commit

---
+++ readme.md
@@ -0,0 +1,11 @@
+## About
+A minimal dependency React app that uses Parcel 2.8
+
+## Install
+`npm install`
+
+## Build
+`npm run build`
+
+## Dev
+`npm run dev`
---
+++ src/hooks/App.js
@@ -0,0 +1,11 @@
+import * as style from '../styles/App.module.scss'
+import Circle from "./Circle"
+
+function App() {
+    return (
+        <div className={ style.container }>
+            <Circle />
+        </div>
+    )
+}
+export default App
